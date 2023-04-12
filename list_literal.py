d60c06cf7f936e9a8889ff1242bb106542bb47f5
deepak2810 <deepakkhanka.97@gmail.com>
2023-04-12 14:15:19 +0530

    ..python literals ...

---
+++ list_literal.py
@@ -0,0 +1,17 @@
+# list literal
+
+fruits = ["apple", "mango", "grapes"]
+print(fruits)
+
+# tuple literals
+
+num = (1,2,3)
+print(num)
+
+#dictionary literals
+alphabets = {'a':"apple", 'b':"boy",'c':'cat'}
+print(alphabets)
+
+#set literal
+vowels = {'a','e','i','o','u'}
+print(vowels)
