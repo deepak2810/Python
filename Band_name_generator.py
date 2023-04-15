47d7cde8829a248558c87de0a2e52a55c096dd72
deepak2810 <deepakkhanka.97@gmail.com>
2023-04-15 17:21:32 +0530

    band_name_generator

---
+++ Band_name_generator.py
@@ -0,0 +1,13 @@
+print(" welcome to the band name generator ! ")
+
+#ask user for the city
+
+city = input("which city did you grpow up : \n")
+
+# ask the user name of their pet.
+
+pet = input("What is the name of your pet? \n")
+
+#print band name.
+
+print("Your band name could be "+ city + " " + pet)
