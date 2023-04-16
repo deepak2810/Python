0cda57764d0f1ca0d605f84946c22c169a6aba6e
deepak2810 <deepakkhanka.97@gmail.com>
2023-04-16 23:45:28 +0530

    ...bmi_calculation...

---
+++ Type_Conversion/Data_types.py
@@ -0,0 +1,29 @@
+# python data types: following are the data types ini python.
+# int
+x = 3
+print(x + 'is a int data type.')
+# floats
+y = 2.345
+print(y + 'is a float data type.')
+# Strings
+
+z = 'Hello_world.'
+print(z + ' is a String data type. ')
+# booleans
+
+c = True
+d = False
+
+print(c + d + ' is a booleabn data type')
+
+# list
+my_list = [2,4,6,8,10]
+print(my_list + ' is a list data type. ')
+
+# tuple
+my_tuple =  (1,2,3,4,5)
+print(my_tuple + 'is a tuple data structure.')
+
+# disctionaries
+my_dict = {'name': 'deepak', 'age': 25}
+print(my_dict + 'is a dict data type')
---
+++ Type_Conversion/bmi_calculator.py
@@ -0,0 +1,17 @@
+# bmi = weight(k.g) * height(m) * height(m)
+
+print('Calculate your BMI:  ___| |____')
+
+weight = int(input('please enter your weight in k.g: '))
+height = float(input('please enter your height in meter : '))
+
+
+print(weight)
+print(height)
+BMI = (weight / (height ** 2))
+
+print(int(BMI))
+
+
+
+
