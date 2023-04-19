a8ab32b13725edf5744ac53e6b4b2db8c87a326f
deepak2810 <deepakkhanka.97@gmail.com>
2023-04-20 02:04:42 +0530

    Fstring in Python

---
+++ Type_Conversion/F_String.py
@@ -0,0 +1,8 @@
+# following program illustrates about 
+
+Age = int(input("plese enter your age: "))
+height = int(input('please enter your height: '))
+isWinning = True
+
+print(f'your age is {Age}, your height is {height}, you are winning is {isWinning}')
+
