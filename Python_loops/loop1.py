5a14c88d7f5d95b9f69d83f1414b99ac73ba5107
deepak2810 <deepakkhanka.97@gmail.com>
2023-05-14 20:22:52 +0530

    sum of all the numbers in the list

---
+++ Python_loops/loop1.py
@@ -0,0 +1,12 @@
+# write a program to calculate the sum of all numbers in the list.
+
+num = [ 10, 20, 30, 40, 50]
+
+sum = 0
+
+for i in num:
+    sum += i
+
+
+print("The sum of the numbers is: ", sum)
+
