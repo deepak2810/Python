da755e94401abfd7e6cd03c27906af5826ab4ced
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-05-22 11:24:31 +0530

    return in functions

---
+++ Functions/fun2.py
@@ -0,0 +1,15 @@
+# creating function with the return value.
+
+def calculator(a,b):
+    return a+b
+
+
+a = int(input('enetr a number : '))
+b = int(input('enter another number to add : '))
+
+
+# function call
+
+result = calculator(a,b)
+
+print(result)
