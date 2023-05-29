e7195552d86878a46bcd2239a97f19f973f7aca7
deepak2810 <deepakkhanka.97@gmail.com>
2023-05-29 11:38:15 +0530

    function practice

---
+++ Python_functions/fun_sum.py
@@ -0,0 +1,20 @@
+def add(a,b,c):
+    d =  a+b+c
+    return d
+    
+    
+
+
+a = int(input('Please enter a num : '))
+b = int(input('Please enter a num : '))
+c = int(input('please ente a  num : '))
+
+
+#function _ call:
+
+e = add(a,b,c)
+
+print(e)
+
+
+
