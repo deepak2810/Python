a4f6861215b0caaf4f93ddf6349211ec8a9b3e33
deepak2810 <deepakkhanka.97@gmail.com>
2023-05-30 13:05:41 +0530

    ....

---
+++ Python_list/list_one.py
@@ -0,0 +1,12 @@
+# program to demonstrate to list slicing in python
+
+# 
+# adding elements to the python.
+
+num = [12, 13, 15, 18]
+
+# using append method
+
+num.append(32)
+
+print("After append: ", num)
