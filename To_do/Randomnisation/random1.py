1b769a3372668d5066a47be8cfbde379c7ecae0b
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-06-03 23:30:32 +0530

    random_py_num generator

---
+++ To_do/Randomnisation/random1.py
@@ -0,0 +1,9 @@
+import random
+
+
+# declairing random integer.
+
+random_int = random.randint(1,10)
+
+print(random_int)
+
---
+++ To_do/test1.py
@@ -0,0 +1,16 @@
+user_prompt =  "Type or add something: "
+
+todos = []
+
+while True:
+    todo = input('type add or show : ')
+    todos.append(todo)
+    print(todos)
+
+
+
+
+
+
+
+ 
