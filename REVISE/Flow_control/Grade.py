4449f0d89d7b5f7fd9b92d1fad89c21050b6b2ad
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-07-16 01:44:35 +0530

    check basics

---
+++ REVISE/Flow_control/Grade.py
@@ -0,0 +1,19 @@
+num = int(input('Please enter your number: '))
+
+
+if num >= 90:
+    print('GRADE: A')
+
+elif num >= 80 and num <= 89:
+    print('GRADE: B')
+
+elif num >= 70 and num <= 79:
+    print('GRADE : C')
+
+
+elif num >= 60 and num <= 69:
+    print('GRADE: D')
+
+
+else:
+    print('FAIL')
