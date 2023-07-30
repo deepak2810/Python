1835b2e71709e798b2a507ab774f8df36c6e9a48
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-07-30 20:44:28 +0530

    traversing inside a list

---
+++ Basics/for2.py
@@ -0,0 +1,7 @@
+# traversing a list within a list using for loop.
+
+Run = [["Virat", 100], ["Hardik", 40], ["Rohit", 50],
+       ["Sanju", 70], ["Ishan", 40], ["Shubhman", 50]]
+
+for player, run in Run:
+    print(player, run)
