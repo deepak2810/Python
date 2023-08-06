1afd61e3052f72045dc4361345ef8a492afa55c6
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-08-06 10:09:25 +0530

    lil start

---
+++ Basics/ex.py
@@ -0,0 +1,7 @@
+# python program to print numbers in a sequence.
+
+for x in range(1, 10):
+    print(x)
+
+
+# this will print the numbers till 9.
---
+++ Basics/for4.py
@@ -0,0 +1,6 @@
+# example explaining nested loop.
+
+for x in range(3):
+    for y in range(1, 10):
+        print(y, end=" ")
+    print()
