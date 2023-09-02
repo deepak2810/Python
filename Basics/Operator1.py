fecb8dc0c318ba892a259e52e5cb399a0095e7b5
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-09-02 17:49:42 +0530

    ...

---
+++ Basics/Operator1.py
@@ -0,0 +1,12 @@
+# Boolean operator >>> "and", "or"
+
+
+num_one = int(input('Please enter a number ____'))
+num_two = int(input('please enter another number_____'))
+
+if num_one * num_two <= 60 and num_one + num_two <= 40:
+    print('You got this lucky , champ')
+
+
+else:
+    print('Better luck next time !')
