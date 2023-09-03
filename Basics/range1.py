8db06d1c2054781b11f8da48d4d5772bf356fd5a
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-09-03 08:41:25 +0530

    ....

---
+++ Basics/range1.py
@@ -0,0 +1,4 @@
+# python program to use range function.
+
+for number in range(1, 11):
+    print(number)
---
+++ Basics/sum_of_num.py
@@ -0,0 +1,9 @@
+# write a python program to display the sum of the all numbers from 1 to n.
+
+total = 0
+
+for i in range(1, 101):
+    total += i
+
+
+print("Sum: ", total)
