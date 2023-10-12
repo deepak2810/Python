0a4cc2b700a0378f35de4ad1b88ee3afe1b18434
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-10-12 20:57:58 +0530

    list

---
+++ Basics/LIST/lst3.py
@@ -0,0 +1,12 @@
+# program to iterate over a list.
+
+ls = []
+
+n = int(input('Please enter the number of elements. '))
+
+for i in range(0, n):
+    ele = int(input())
+    ls.append(ele)
+
+
+print(ls)
