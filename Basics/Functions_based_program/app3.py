d43ff236aac723e37d152b8cf700a4f6d4864a65
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-10-17 16:01:22 +0530

    ...

---
+++ Basics/Functions_based_program/app3.py
@@ -0,0 +1,15 @@
+# basic program to add three numbers using function.
+
+# function_declaration.
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+num1 = 5
+num2 = 10
+
+
+result = add_numbers(num1, num2)
+
+print(result)
---
+++ Basics/Functions_based_program/app_2_argu.py
@@ -0,0 +1,2 @@
+def greet(first_name, last_name):
+    print(f'Hi' {first})
