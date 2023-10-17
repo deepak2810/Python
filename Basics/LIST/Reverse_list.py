d457e2ca88331e6c41b9cfab6bd10ec9446097ed
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-10-17 13:30:56 +0530

    ....

---
+++ Basics/LIST/Reverse_list.py
@@ -0,0 +1,13 @@
+# python program to reverse a list.
+
+ls = []
+
+n = int(input('Enter the size of the list'))
+
+
+for i in range(0, n):
+    a = int(input('Enter element: '))
+    ls.append(a)
+
+
+print('Given list is : ', + ls)
