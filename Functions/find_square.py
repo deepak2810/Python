3f9328e3915d8c7661effe247d2d001b65eff47a
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-10-30 11:12:42 +0530

    to find the square of a given program

---
+++ Functions/find_square.py
@@ -0,0 +1,13 @@
+# Python program to find the square of a given program.
+
+
+def square(num):
+    ans = num * num
+    return ans
+
+
+# calling the function.
+
+a = int(input('please enter a number to find its square '))
+
+print(square(a))
