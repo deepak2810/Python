36edbe01dff35ac7e288380d7933ad2138a0a862
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-11-25 01:41:43 +0530

    slicing the given list

---
+++ Basics/Type_conversion_python/tcp3.py
@@ -0,0 +1,8 @@
+# python program to slice the given list.
+
+ls1 = [1, 'ABC', 2, 3, 'XYZ', 'D', 'K']
+
+
+print('The first half of list is ', ls1[:3])
+
+print('The first half of list is ', ls1[3:])
