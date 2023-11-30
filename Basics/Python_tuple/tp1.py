b98d5fd3d195febcbd46a91f53dee3695d04d5f6
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2023-12-01 00:05:13 +0530

    tuples in python

---
+++ Basics/Python_tuple/tp1.py
@@ -0,0 +1,13 @@
+# Python tuple : tuple is similar to a python list it is just that we can not change the tuple content
+# is assigned.
+
+
+# 1: creating a empty tuple.
+
+tup = ()
+print('Empty tup:  ', tup)
+
+
+# 2: tuples having integer.
+my_tup = (1, 2, 3, 4, 5)
+print(my_tup)
