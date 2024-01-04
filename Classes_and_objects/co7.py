4bc9154c873f5a16b977ed56b93e854ddd2c31a1
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2024-01-05 03:02:12 +0530

    ..

--- Classes_and_objects/co7.py
+++ Classes_and_objects/co7.py
@@ -15,14 +15,3 @@ Shyam = Student('Shyam')
 
 Ram.speak()
 Shyam.speak()
-
-
-
-#
-1.  Check the boot policy of server 12 to make sure you see the M2 RAID is there. Compare the boot policy with a working M6 server.
-2.  Boot the server and launch KVM. Once KVM shows the server boot into UEFI shell, type “map” and confirm you have a “fs0” bootpartition.
-
-
-
-
-
